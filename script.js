b8c7c28bf980512b4991865445fe89b51f099e89
Lúcia de Fátima <102824924+luciafm@users.noreply.github.com>
2023-08-12 14:06:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,328 @@
+const listaTarefas = [];
+
+function adicionarTarefa() {
+  const inputTarefa = document.getElementById("tarefa");
+  const tarefa = inputTarefa.value;
+
+  if (tarefa !== "") {
+    const novoItem = document.createElement("li");
+    novoItem.textContent = tarefa;
+    listaTarefas.push(tarefa);
+    atualizarLista();
+    inputTarefa.value = "";
+
+    console.log("Tarefa adicionada:", tarefa);
+  }
+}
+
+function atualizarLista() {
+  const listaTarefasElement = document.getElementById("listaTarefas");
+  listaTarefasElement.innerHTML = "";
+  for (const tarefa of listaTarefas) {
+    const novoItem = document.createElement("li");
+    novoItem.textContent = tarefa;
+    listaTarefasElement.appendChild(novoItem);
+  }
+
+  console.log("Lista de tarefas completa:", listaTarefas);
+}
+
+const openButton = document.getElementById("openPopup");
+const closeButton = document.getElementById("closePopup");
+
+function abrir() {
+  popup.style.display = "flex";
+}
+function abrir2() {
+  popup2.style.display = "flex";
+}
+function abrir3() {
+  popup3.style.display = "flex";
+}
+function abrir4() {
+  popup4.style.display = "flex";
+}
+function fechar() {
+  popup.style.display = "none";
+  popup2.style.display = "none";
+  popup3.style.display = "none";
+  popup4.style.display = "none";
+}
+
 class Node {
-      constructor(name) {
-        this.name = name;
-        this.left = null;
-        this.right = null;
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(key) {
+    this.root = this._insert(this.root, key);
+  }
+
+  _insert(node, key) {
+    if (node === null) {
+      return new Node(key);
+    }
+
+    if (key < node.key) {
+      node.left = this._insert(node.left, key);
+    } else {
+      node.right = this._insert(node.right, key);
+    }
+
+    return node;
+  }
+
+  size() {
+    return this._size(this.root);
+  }
+
+  _size(node) {
+    if (node === null) {
+      return 0;
+    }
+    return 1 + this._size(node.left) + this._size(node.right);
+  }
+
+  height() {
+    return this._height(this.root);
+  }
+
+  _height(node) {
+    if (node === null) {
+      return -1;
+    }
+    return (
+      1 + Math.max(this._height(node.left), this._height(node.right))
+    );
+  }
+
+  min() {
+    return this._min(this.root);
+  }
+
+  _min(node) {
+    if (node === null) {
+      return null;
+    }
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node.key;
+  }
+
+  max() {
+    return this._max(this.root);
+  }
+
+  _max(node) {
+    if (node === null) {
+      return null;
+    }
+    while (node.right !== null) {
+      node = node.right;
+    }
+    return node.key;
+  }
+
+  internalPathLength() {
+    return this._internalPathLength(this.root, 0);
+  }
+
+  _internalPathLength(node, depth) {
+    if (node === null) {
+      return 0;
+    }
+    return (
+      depth +
+      this._internalPathLength(node.left, depth + 1) +
+      this._internalPathLength(node.right, depth + 1)
+    );
+  }
+
+  isBalanced() {
+    return this._isBalanced(this.root);
+  }
+
+  _isBalanced(node) {
+    if (node === null) {
+      return true;
+    }
+
+    const leftHeight = this._height(node.left);
+    const rightHeight = this._height(node.right);
+
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return false;
+    }
+
+    return this._isBalanced(node.left) && this._isBalanced(node.right);
+  }
+
+  // Adicione os métodos de travessia aqui
+  inorderTraversal() {
+    const result = [];
+    this._inorderTraversal(this.root, result);
+    return result;
+  }
+
+  _inorderTraversal(node, result) {
+    if (node !== null) {
+      this._inorderTraversal(node.left, result);
+      result.push(node.key);
+      this._inorderTraversal(node.right, result);
+    }
+  }
+
+  preorderTraversal() {
+    const result = [];
+    this._preorderTraversal(this.root, result);
+    return result;
+  }
+
+  _preorderTraversal(node, result) {
+    if (node !== null) {
+      result.push(node.key);
+      this._preorderTraversal(node.left, result);
+      this._preorderTraversal(node.right, result);
+    }
+  }
+
+  postorderTraversal() {
+    const result = [];
+    this._postorderTraversal(this.root, result);
+    return result;
+  }
+
+  _postorderTraversal(node, result) {
+    if (node !== null) {
+      this._postorderTraversal(node.left, result);
+      this._postorderTraversal(node.right, result);
+      result.push(node.key);
+    }
+  }
+
+  levelOrderTraversal() {
+    const result = [];
+    const queue = [];
+
+    if (this.root !== null) {
+      queue.push(this.root);
+    }
+
+    while (queue.length > 0) {
+      const node = queue.shift();
+      result.push(node.key);
+
+      if (node.left !== null) {
+        queue.push(node.left);
+      }
+
+      if (node.right !== null) {
+        queue.push(node.right);
       }
     }
 
-    class BST {
-      constructor() {
-        this.root = null;
-      }
+    return result;
+  }
 
-      insert(name) {
-        this.root = this._insert(this.root, name);
-      }
+  // ... (outros métodos da árvore binária)
+}
 
-      _insert(node, name) {
-        if (node === null) {
-          return new Node(name);
-        }
+const tree = new BST();
 
-        if (name < node.name) {
-          node.left = this._insert(node.left, name);
-        } else {
-          node.right = this._insert(node.right, name);
-        }
+function adicionarTarefa() {
+  const inputTarefa = document.getElementById("tarefa");
+  const tarefa = inputTarefa.value.trim();
 
-        return node;
-      }
+  if (tarefa !== "") {
+    tree.insert(parseInt(tarefa));
+    atualizarLista();
+    mostrarInformacoes();
+    inputTarefa.value = "";
 
-      // Outros métodos da árvore binária
+    console.log("Tarefa adicionada:", tarefa);
+  }
 
-      inorderTraversalNames() {
-        const result = [];
-        this._inorderTraversalNames(this.root, result);
-        return result;
-      }
+  atualizarLista();
+  mostrarInformacoes();
+  desenharArvore();
+  inputTarefa.value = "";
 
-      _inorderTraversalNames(node, result) {
-        if (node !== null) {
-          this._inorderTraversalNames(node.left, result);
-          result.push(node.name);
-          this._inorderTraversalNames(node.right, result);
-        }
-      }
+  console.log("Tarefa adicionada:", tarefa);
+}
 
-      // Outros métodos da árvore binária
-    }
+function atualizarLista() {
+  const listaTarefasElement = document.getElementById("listaTarefas");
+  listaTarefasElement.innerHTML = "";
 
-    const tree = new BST();
+  treeTraversal(tree.root, (key) => {
+    const novoItem = document.createElement("li");
+    novoItem.textContent = key;
+    listaTarefasElement.appendChild(novoItem);
+  });
 
-    function adicionarTarefa() {
-      const inputTarefa = document.getElementById("tarefa");
-      const tarefa = inputTarefa.value.trim();
+  console.log("Árvore de tarefas completa:", tree.root);
+}
 
-      if (tarefa !== "") {
-        tree.insert(tarefa);
-        atualizarLista();
-        mostrarInformacoes();
-        desenharArvore();
-        inputTarefa.value = "";
+function treeTraversal(node, callback) {
+  if (node !== null) {
+    treeTraversal(node.left, callback);
+    callback(node.key);
+    treeTraversal(node.right, callback);
+  }
+}
 
-        console.log("Tarefa adicionada:", tarefa);
-      }
-    }
+function mostrarInformacoes() {
+  const tamanhoElemento = document.getElementById("tamanho");
+  const alturaElemento = document.getElementById("altura");
+  const menorElemento = document.getElementById("menor");
+  const maiorElemento = document.getElementById("maior");
 
-    // Funções de atualização da interface
-    function atualizarLista() {
-      const listaTarefasElement = document.getElementById("listaTarefas");
-      listaTarefasElement.innerHTML = "";
+  const comprimentoInternoElemento =
+    document.getElementById("comprimentoInterno");
+  const balanceamentoElemento = document.getElementById("balanceamento");
+  const travessiasElemento = document.getElementById("travessias");
 
-      const nomesOrdenados = tree.inorderTraversalNames();
-      nomesOrdenados.forEach(nome => {
-        const novoItem = document.createElement("div");
-        novoItem.textContent = nome;
-        listaTarefasElement.appendChild(novoItem);
-      });
-    }
+  tamanhoElemento.textContent = `Tamanho da Árvore: ${tree.size()}`;
+  alturaElemento.textContent = `Altura da Árvore: ${tree.height()}`;
+  menorElemento.textContent = `Menor Elemento: ${tree.min()}`;
+  maiorElemento.textContent = `Maior Elemento: ${tree.max()}`;
+  comprimentoInternoElemento.textContent = `Comprimento Interno: ${tree.internalPathLength()}`;
+  balanceamentoElemento.textContent = `Árvore Balanceada: ${
+    tree.isBalanced() ? "Sim" : "Não"
+  }`;
+
+  const inorder = tree.inorderTraversal().join(", ");
+  const preorder = tree.preorderTraversal().join(", ");
+  const postorder = tree.postorderTraversal().join(", ");
+  const levelorder = tree.levelOrderTraversal().join(", ");
+
+  travessiasElemento.innerHTML =
+    `Travessia em Ordem:<br>${inorder}<br>` +
+    `<br>`+
+    `Travessia Pré-Ordem:<br>${preorder}<br>` +
+    `<br>`+
+    `Travessia Pós-Ordem:<br>${postorder}<br>` +
+    `<br>`+
+    `Travessia Level Order:<br>${levelorder}`;
+}
 
 function desenharArvore() {
   const arvoreContainer = document.getElementById("arvore-container");
   const inseridosDiv = document.querySelector(".inseridos");
 
   const width = inseridosDiv.clientWidth;
-  const height = calcularAlturaArvore(tree.root) * 70 + 100;
+  const height = calcularAlturaArvore(tree.root) * 70 + 100; // Ajuste aqui
 
-  arvoreContainer.innerHTML = "";
+  arvoreContainer.innerHTML = ""; // Limpar o conteúdo anterior
 
   const svgNS = "http://www.w3.org/2000/svg";
   const svg = document.createElementNS(svgNS, "svg");
@@ -102,7 +339,7 @@ function desenharArvore() {
   }
 
   arvoreContainer.appendChild(svg);
-  inseridosDiv.style.height = `${height}px`;
+  inseridosDiv.style.height = `${height}px`; // Ajustar a altura da div
 }
 
 function calcularAlturaArvore(node) {
@@ -150,7 +387,7 @@ function desenharNo(svg, node, x, y, xSpacing, ySpacing) {
   circle.setAttribute("cx", x);
   circle.setAttribute("cy", y);
   circle.setAttribute("r", 20);
-  circle.setAttribute("fill", "#FFBAAA");
+  circle.setAttribute("fill", "#FFBAAA"); // Definindo a cor como #FFBAAA
   svg.appendChild(circle);
 
   const text = document.createElementNS(
@@ -160,7 +397,7 @@ function desenharNo(svg, node, x, y, xSpacing, ySpacing) {
   text.setAttribute("x", x);
   text.setAttribute("y", y + 5);
   text.setAttribute("text-anchor", "middle");
-  text.textContent = node.name;
+  text.textContent = node.key;
   svg.appendChild(text);
 }
 
@@ -175,20 +412,4 @@ function drawLine(x1, y1, x2, y2) {
   line.setAttribute("y2", y2);
   line.setAttribute("stroke", "black");
   return line;
-}
-
-function mostrarInformacoes() {
-  const tamanhoElemento = document.getElementById("tamanho");
-  const alturaElemento = document.getElementById("altura");
-  const menorElemento = document.getElementById("menor");
-  const maiorElemento = document.getElementById("maior");
-
-  const nomesOrdenados = tree.inorderTraversalNames();
-  const primeiraLetra = nomesOrdenados[0][0];
-  const ultimaLetra = nomesOrdenados[nomesOrdenados.length - 1][0];
-
-  tamanhoElemento.textContent = `Tamanho da Árvore: ${tree.size()}`;
-  alturaElemento.textContent = `Altura da Árvore: ${tree.height()}`;
-  menorElemento.textContent = `Primeira Letra: ${primeiraLetra}`;
-  maiorElemento.textContent = `Última Letra: ${ultimaLetra}`;
 }
